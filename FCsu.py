3567212651925b7632674820377342919f28d0c4
0penBrain <48731257+0penBrain@users.noreply.github.com>
2021-05-18 19:29:13 +0200

    Split fontsizer as a package

--- FCsu.py
+++ FCsu.py
@@ -217,61 +217,6 @@ class statusBarWid(QtGui.QWidget):
         lay.setContentsMargins(0,0,0,0)
         self.setLayout(lay)
 
-##### Dock widget font sizer
-
-class DockWFontSizer(QtCore.QObject):
-
-    def __init__(self, dockWidName):
-        super(DockWFontSizer, self).__init__()
-        self.dWid = Gui.getMainWindow().findChild(QtGui.QDockWidget, dockWidName)
-        if self.dWid:
-            initialPS = QtGui.QApplication.font().pointSize()
-            self.font = QtGui.QFont(self.dWid.font())
-            self.titleWid = QtGui.QWidget()
-            lay = QtGui.QHBoxLayout()
-            self.sliFS = QtGui.QSlider(QtCore.Qt.Horizontal)
-            self.sliFS.setSliderPosition(initialPS)
-            self.sliFS.setMaximum(initialPS)
-            self.sliFS.setMinimum(int(initialPS/2))
-            self.sliFS.setPageStep(1)
-            self.sliFS.valueChanged.connect(self.changeFS)
-            self.sliFS.sliderReleased.connect(self.changeFS)
-            lay.addWidget(QtGui.QLabel("Size : "))
-            lay.addWidget(self.sliFS)
-            self.titleWid.setLayout(lay)
-            self.timer = QtCore.QTimer(self)
-            self.timer.setInterval(2500)
-            self.timer.timeout.connect(self.titleBarTO)
-            self.dWid.installEventFilter(self)
-            setattr(self.dWid, 'fontSizer', self)
-
-    @staticmethod
-    def changeFSRec(wid, fs):
-        if hasattr(wid, 'font') and wid.font().pointSize() != fs and str(wid.__class__).find('QAction') == -1:
-            newFont = QtGui.QFont(wid.font())
-            newFont.setPointSize(fs)
-            wid.setFont(newFont)
-            for childWid in wid.children():
-                DockWFontSizer.changeFSRec(childWid, fs)
-
-    def changeFS(self, fs=0):
-        self.timer.start()
-        if fs != 0:
-            DockWFontSizer.changeFSRec(self.dWid, fs)
-
-    def eventFilter(self, obj, event):
-        if event.type() == QtCore.QEvent.MouseButtonPress and event.buttons() == QtCore.Qt.MidButton:
-            self.dWid.setTitleBarWidget(self.titleWid)
-            self.timer.start()
-            return True
-        else:
-            return False
-
-    def titleBarTO(self):
-        if not self.sliFS.isSliderDown():
-            self.dWid.setTitleBarWidget(None)
-            self.timer.stop()
-
 class AboutInfo(QtCore.QObject):
     def eventFilter(self, obj, ev):
         if obj.metaObject().className() == 'Gui::Dialog::AboutDialog':
@@ -307,8 +252,9 @@ def run():
         Gui.getMainWindow().statusBar().setVisible(True)
         
     if cfg.fontSizerEnabled:
+        from fontsizer import fontsizer
         for dwid in Gui.getMainWindow().findChildren(QtGui.QDockWidget):
-            DockWFontSizer(dwid.objectName())
+            fontsizer.DockWFontSizer(dwid.objectName())
 
 if __name__ == '__main__':
     run()
