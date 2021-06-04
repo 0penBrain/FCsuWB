ab551bc42b7959d13c5464726479c45fc2c1bc25
0penBrain <48731257+0penBrain@users.noreply.github.com>
2021-06-04 18:09:02 +0200

    Dock widget closer can now be used as a toggler

--- statusbar/helpers/closers.py
+++ statusbar/helpers/closers.py
@@ -1,7 +1,30 @@
 from PySide2 import QtWidgets
 import FreeCADGui as Gui
 
+dwstate = []
+
+def saveDWState(overwrite = False):
+    global dwstate
+    if not dwstate or overwrite:
+        dwstate = [dw for dw in Gui.getMainWindow().findChildren(QtWidgets.QDockWidget) if dw.isVisible()]
+
+def restoreDWState(clear = True):
+    global dwstate
+    if dwstate:
+        for dw in dwstate:
+            dw.show()
+        if clear:
+            dwstate.clear()
+
 def closeAllDWButCV():
-    for dwid in Gui.getMainWindow().findChildren(QtWidgets.QDockWidget):
-        if dwid.objectName() != 'Combo View':
-            dwid.hide()
+    global dwstate
+    dws = Gui.getMainWindow().findChildren(QtWidgets.QDockWidget)
+    if len(dws) == 1 and dws[0].objectName() == 'Combo View' or dwstate:
+        restoreDWState()
+    else:
+        saveDWState()
+        for dw in dws:
+            if dw.objectName() != 'Combo View':
+                dw.hide()
+
+Gui.getMainWindow().mainWindowClosed.connect(restoreDWState) #FIXME : signal is sent after window settings are saved
